719e06b502c818551026a533a8f2713fc84a42b4
Ashish Singh <45603733+siashish@users.noreply.github.com>
2023-06-07 15:37:37 +0000

    update script file

--- fibo-go/script.sh
+++ fibo-go/script.sh
@@ -6,14 +6,17 @@ date
 echo "======================================="
 echo "wasmer"
 time docker run -it docker.io/library/wasmer-go-fibo
+\time -f "Program: %C\nTotal time: %E\nUser Mode (s) %U\nKernel Mode (s) %S\nCPU: %P\nMemory: %K\nRSS: %M" docker run -it docker.io/library/wasmer-go-fibo
 echo "======================================="
 echo "wasmtime"
 time docker run -it docker.io/library/wasmtime-go-fibo
+\time -f "Program: %C\nTotal time: %E\nUser Mode (s) %U\nKernel Mode (s) %S\nCPU: %P\nMemory: %K\nRSS: %M" docker run -it docker.io/library/wasmtime-go-fibo
 echo "======================================="
 
 echo "======================================="
 echo "wasmedge"
 time docker run -it docker.io/library/wasmedge-go-fibo
+\time -f "Program: %C\nTotal time: %E\nUser Mode (s) %U\nKernel Mode (s) %S\nCPU: %P\nMemory: %K\nRSS: %M" docker run -it docker.io/library/wasmedge-go-fibo
 echo "======================================="
 echo
 echo
--- fibo-rust/script.sh
+++ fibo-rust/script.sh
@@ -6,13 +6,16 @@ date
 echo "======================================="
 echo "wasmer"
 time docker run -it docker.io/library/wasmer-rust-fibo
+\time -f "Program: %C\nTotal time: %E\nUser Mode (s) %U\nKernel Mode (s) %S\nCPU: %P\nMemory: %K\nRSS: %M" docker run -it docker.io/library/wasmer-rust-fibo
 echo "======================================="
 echo "wasmtime"
 time docker run -it docker.io/library/wasmtime-rust-fibo
+\time -f "Program: %C\nTotal time: %E\nUser Mode (s) %U\nKernel Mode (s) %S\nCPU: %P\nMemory: %K\nRSS: %M" docker run -it docker.io/library/wasmtime-rust-fibo
 
 echo "======================================="
 echo "wasmedge"
 time docker run -it docker.io/library/wasmedge-rust-fibo
+\time -f "Program: %C\nTotal time: %E\nUser Mode (s) %U\nKernel Mode (s) %S\nCPU: %P\nMemory: %K\nRSS: %M" docker run -it docker.io/library/wasmedge-rust-fibo
 echo "======================================="
 echo
 echo
